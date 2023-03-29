3e089c3842b0c919cae9319fba024372f66a1aaa
Alicia Moses <aliciamoses.dev@gmail.com>
2023-03-30 00:55:52 +0100

    Passing tests for line numbers in editor area Line numbers update with each line with current line highlighted

--- app/editor.py
+++ app/editor.py
@@ -1,13 +1,32 @@
-from PyQt6.QtWidgets import QPlainTextEdit
+from PyQt6.QtWidgets import QPlainTextEdit, QWidget, QTextEdit
+from PyQt6.QtCore import Qt, QRect, QSize
+from PyQt6.QtGui import QPainter, QColor, QTextFormat
+
+class LineNumberArea(QWidget):
+    def __init__(self, editor):
+        super().__init__(editor)
+        self.editor = editor
+
+    def sizeHint(self):
+        return QSize(self.editor.line_number_area_width(), 0)
+
+    def paintEvent(self, event):
+        self.editor.line_number_area_paint_event(event)
 
 class Editor(QPlainTextEdit):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.set_editor()
-        
+        self.line_number_area = LineNumberArea(self)
+
+        self.blockCountChanged.connect(self.update_line_number_area_width)
+        self.updateRequest.connect(self.update_line_number_area)
+        self.cursorPositionChanged.connect(self.highlight_current_line)
+
+        self.update_line_number_area_width(0)
 
     def set_editor(self):
-        self.set_font() 
+        self.set_font()
 
     def set_font(self):
         doc = self.document()
@@ -16,5 +35,65 @@ class Editor(QPlainTextEdit):
         font.setPointSize(11)
         doc.setDefaultFont(font)
 
+    def line_number_area_width(self):
+        digits = 1
+        max_count = max(1, self.blockCount())
+        while max_count >= 10:
+            max_count //= 10
+            digits += 1
 
-    
+        space = 3 + self.fontMetrics().horizontalAdvance('9') * digits
+        return space
+
+    def update_line_number_area_width(self, new_block_count):
+        self.setViewportMargins(self.line_number_area_width(), 0, 0, 0)
+
+    def update_line_number_area(self, rect, dy):
+        if dy:
+            self.line_number_area.scroll(0, dy)
+        else:
+            self.line_number_area.update(0, rect.y(), self.line_number_area.width(), rect.height())
+
+        if rect.contains(self.viewport().rect()):
+            self.update_line_number_area_width(0)
+
+    def line_number_area_paint_event(self, event):
+        painter = QPainter(self.line_number_area)
+        painter.fillRect(event.rect(), Qt.GlobalColor.lightGray)
+
+        block = self.firstVisibleBlock()
+        block_number = block.blockNumber()
+        top = round(self.blockBoundingGeometry(block).translated(self.contentOffset()).top())
+        bottom = top + round(self.blockBoundingRect(block).height())
+
+        while block.isValid() and top <= event.rect().bottom():
+            if block.isVisible() and bottom >= event.rect().top():
+                number = str(block_number + 1)
+                painter.setPen(Qt.GlobalColor.black)
+                painter.drawText(0, top, self.line_number_area.width(), self.fontMetrics().height(),
+                                 Qt.AlignmentFlag.AlignRight, number)
+
+            block = block.next()
+            top = bottom
+            bottom = top + round(self.blockBoundingRect(block).height())
+            block_number += 1
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+
+        cr = self.contentsRect()
+        self.line_number_area.setGeometry(QRect(cr.left(), cr.top(), self.line_number_area_width(), cr.height()))
+
+    def highlight_current_line(self):
+        extra_selections = []
+
+        if not self.isReadOnly():
+            selection = QTextEdit.ExtraSelection()
+            line_color = QColor(Qt.GlobalColor.yellow).lighter(160)
+            selection.format.setBackground(line_color)
+            selection.format.setProperty(QTextFormat.Property.FullWidthSelection, True)
+            selection.cursor = self.textCursor()
+            selection.cursor.clearSelection()
+            extra_selections.append(selection)
+
+        self.setExtraSelections(extra_selections)
--- tests/test_editor.py
+++ tests/test_editor.py
@@ -32,6 +32,21 @@ class TestEditor(unittest.TestCase):
         font_size = font.pointSize()
         self.assertEqual(font_size, 11)
 
+    def test_line_numbers_visible(self):
+        line_number_area = self.editor.line_number_area
+        self.assertIsNotNone(line_number_area)
+
+    def test_line_number_area_width(self):
+        self.editor.setPlainText("1\n2\n3\n4\n5\n6\n7\n8\n9\n10")
+        self.assertEqual(self.editor.line_number_area_width(), 3 + self.editor.fontMetrics().horizontalAdvance('9') * 2)
+
+    def test_line_numbers_update_on_new_line(self):
+        self.editor.setPlainText("1\n2\n3")
+        initial_width = self.editor.line_number_area_width()
+        self.editor.setPlainText("1\n2\n3\n4\n5\n6\n7\n8\n9\n10")
+        updated_width = self.editor.line_number_area_width()
+        self.assertNotEqual(initial_width, updated_width)
+
 
 if __name__ == '__main__':
     unittest.main()
